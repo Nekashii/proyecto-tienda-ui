914e5288e5da26fb9896d1ed711ae1aa871e127e
Rebecca C <nekashii@outlook.com>
2024-12-01 14:35:56 -0600

    feat: enclose session logic

--- session.js
+++ session.js
@@ -1,12 +1,14 @@
-const nameContainer = document.getElementById('nameContainer')
-const logoutButton = document.getElementById('logoutButton')
+;(() => {
+  const nameContainer = document.getElementById('nameContainer')
+  const logoutButton = document.getElementById('logoutButton')
 
-const { storageKey } = config
-const { firstName, lastName } = JSON.parse(localStorage.getItem(`${storageKey}.auth.user`))
+  const { storageKey } = config
+  const { firstName, lastName } = JSON.parse(localStorage.getItem(`${storageKey}.auth.user`))
 
-if (nameContainer) nameContainer.innerText = `${firstName} ${lastName}`
-if (logoutButton)
-  logoutButton.onclick = () => {
-    localStorage.removeItem(`${storageKey}.auth.user`)
-    window.location.replace('/')
-  }
+  if (nameContainer) nameContainer.innerText = `${firstName} ${lastName}`
+  if (logoutButton)
+    logoutButton.onclick = () => {
+      localStorage.removeItem(`${storageKey}.auth.user`)
+      window.location.replace('/')
+    }
+})()
